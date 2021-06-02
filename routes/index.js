f422228a36bac962f760b703dab57a74f31d9760
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-06-02 14:36:40 -0500

    Remove unnecessary files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({
+    app: 'Fake Generator API',
+    currentDate: new Date()
+  });
 });
 
 module.exports = router;
